a9ebb4647814cc74508b36e808cb72629eaef725
decypher1 <mumekwe2@gmail.com>
2022-10-29 12:50:55 +0100

    removed the called function

--- db.js
+++ db.js
@@ -1,1 +1,21 @@
 const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema;
+require("dotenv").config();
+
+const mongodbConnectionString = process.env.mongodbConnectionString;
+
+const connectToMongoDB = () => {
+  mongoose.connect(mongodbConnectionString);
+
+  mongoose.connection.on("connected", () => {
+    console.log("Successfully Connected to MongoDB");
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.log("Error Connecting to MongoDB", err);
+  });
+};
+
+
+module.exports = connectToMongoDB;
